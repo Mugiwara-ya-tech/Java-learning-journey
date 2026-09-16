ba3bba50947c7f28806961e32475dba27b5424e4
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-09-16 17:28:14 +0530

    Basic

---
+++ program_structure.java
@@ -0,0 +1,21 @@
+class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hi there");    # O/P is Hi there
+    }
+}
+
+# The first line of the code defines a class called Demo
+# In java every line of code that can actually runs needs to be inside a class you can call the class anything you want.
+
+# The class opens & closes using curly brackets any code that we want to include in the class needs to go inside the brackets.
+
+# As you can see the Demo class includes the following line
+    public static void main(String[] args) {
+
+    }
+
+# In java each application has an entry point or a starting point which is a method called main
+# Notices that the main method similar to the class, open and closes with curly brackets
+
+# The main method contain the code that execute when we run our program.
+# The main method can contain multiple statements
