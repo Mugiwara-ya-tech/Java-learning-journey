a5e04c2a5c070214f5d881b133364468f7e6ed59
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-09-16 17:27:58 +0530

    Initial commit

---
+++ introduction.java
@@ -0,0 +1,32 @@
+# The print statement in Java is as following
+
+System.out.println("Hello, World!");
+
+# The print instruction needs to be followed by parenthesis
+
+# Multiple Statement
+# A line of code is called a statement. A statement performs a specific task.
+# The output command is an eg of statement
+
+# Each statement needs to end with a semicolon ;
+
+# You can add as many statement (or line of code) as you need
+public class Demo {
+    public static void main(String[] args){
+        System.out.println("Name :");        # O/P is Name:
+        System.out.println("Surname:");      # O/P is Surname:
+    }
+}
+
+# When you give the computer many statement the instructions will be executed line by line from top to bottom
+
+# Computer treat text & numbers differently when printing text outputs you need to enclose the text in double quotes, you dont need quotes when outputting numbers
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Points:");        # O/P is Points:
+        System.out.println(500);              # O/P is 500
+    }
+}
+
+# Java is a case sensitive language
