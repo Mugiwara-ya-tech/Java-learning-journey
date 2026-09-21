a6fe8336c4abd858dfee6c0b6a38aff260790b17
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-09-21 19:01:28 +0530

    Fix comments and formatting in variables.java

--- variables.java
+++ variables.java
@@ -1,44 +1,44 @@
-# A variables lets you store a value by assigning it to a name. The name can be used to refer to the value later in the program
-# Every variable has a type which defines the type of the value it holds. A variable can holds a text value, a number, a decimal etc
+// A variables lets you store a value by assigning it to a name. The name can be used to refer to the value later in the program
+// Every variable has a type which defines the type of the value it holds. A variable can holds a text value, a number, a decimal etc
 
 "This is some text"
-# Text in quote is called a string
+// Text in quote is called a string
 
 String name;
-# This creates a variable called name or type string
-# Now our variable name can hold string values
+// This creates a variable called name or type string
+//Now our variable name can hold string values
 
-# In programming terms the process of creating a variable is called declaration
+// In programming terms the process of creating a variable is called declaration
 
-# After declaring our variable we can assign it a value using the assignment operator "="
+//After declaring our variable we can assign it a value using the assignment operator "="
 String name;
 name = "James";
 
-# Now name hold the value "James"
+// Now name hold the value "James"
 
-# Note: The type String should start with a capital letter S. A lowercase version will cause an error
+// Note: The type String should start with a capital letter S. A lowercase version will cause an error
 
 class Demo{
     public static void main(String[] args) {
         String name;
         name = "James"
-        System.out.println(name);    # O/P is James
+        System.out.println(name);    // O/P is James
     }
 }
 
-# We can combine the declaration and assignment into one statement
+// We can combine the declaration and assignment into one statement
 class Demo{
     public static void main(String[] args) {
         String name = "James";
-        System.out.println(name);    # O/P is James
+        System.out.println(name);    // O/P is James
     }
 }
 
-# A variable can change its value during the program multiple times
+// A variable can change its value during the program multiple times
 class Demo{
     public static void main(String[] args) {
         String name = "James";
         name = "David";
-        System.out.println(name)   # O/P is David
+        System.out.println(name)   // O/P is David
     }
 }
